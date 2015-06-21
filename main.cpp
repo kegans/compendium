0584ce994fc95b8d409be9dda73548fd1d451476
Kegan <kegan.sanchez@gmail.com>
2015-06-21 15:19:45 -0700

    Updates character variables

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@
   * kegan.sanchez@gmail.com
   * 
   * Character-Lexicon: Creates a binary search tree ADT to 
-  * manage mobile apps by implementing functions to 
+  * manage RPG Characters by implementing functions to 
   * recursively insert, retrieve, display, remove all 
   * matches by location, and remove all items.
+
   *
   * main.cpp module: serves as the client interface of
   * the program by including and creating an instance
@@ -31,81 +32,81 @@ int main()
     //initialize temporary variables
     char name[100]; //for an app's name
     char temp[100]; //for a single keyword
-    char ** keywords; //for an array of keywords
-    char descrip[300]; //for an app's description
-    int rating; //for a numerical app rating
-    int cat_num; //for the number of keywords/categories
+    char ** skills; //for an array of skills
+    char arcana[300]; //for an app's arcanation
+    int level; //for a numerical app level
+    int skills_num; //for the number of skills/categories
     
     do
     { 
         do
         {
             //gets the name of the app
-            cout <<"Please enter the APP name: ";
+            cout <<"Please enter the character's name: ";
             cin.get(name, 100); cin.ignore(100, '\n');
 
             //gets the number of categories then get each category
-            cout <<"How many keyword/categories would you like to enter" << '\n'<< "(between 1 and 5): ";
-            cin >> cat_num; cin.ignore(100, '\n');
-	    keywords = new char * [cat_num];
-            for(int k = 0; k < cat_num; ++k)
+            cout <<"How many skills would you like to enter" << '\n'<< "(between 1 and 5): ";
+            cin >> skills_num; cin.ignore(100, '\n');
+	    skills = new char * [skills_num];
+            for(int k = 0; k < skills_num; ++k)
             {
-                cout <<"Please enter a category: ";
+                cout <<"Please enter a skill: ";
                 cin.get(temp, 35);
                 cin.ignore(100, '\n');
-	        keywords[k] = new char[strlen(temp)+1];
-                strcpy(keywords[k], temp);
+	        skills[k] = new char[strlen(temp)+1];
+                strcpy(skills[k], temp);
             }
 
-            //gets the description
-            cout <<"Description: ";
-            cin.get(descrip, 300); cin.ignore(100, '\n');
-            //gets the rating
-            cout <<"Rating (between 1 and 10): ";
-            cin >> rating; cin.ignore(100,'\n');
+            //gets the arcana
+            cout <<"Arcana: ";
+            cin.get(arcana, 300); cin.ignore(100, '\n');
+            //gets the level
+            cout <<"Level (between 1 and 10): ";
+            cin >> level; cin.ignore(100,'\n');
 
             //creates and inserts an entry into the BST 
-            if(!a_list.create(name, keywords, descrip, rating, cat_num))
+            if(!a_list.create(name, skills, arcana, level, skills_num))
                 cout << "Could not insert entry." << endl;
 
         }while(again()); //get app info while client wants to continue  
 
         //display the bst through inorder traversal
-        cout <<"\nThese are the contents of the APP BST";
+        cout <<"\nThese are the contents of the compendium";
         cout <<"\nin sorted order:\n\n";
         if(!a_list.display_all())
-            cout <<"Could not display the APP list." << endl;
+            cout <<"Could not display the compendium." << endl;
 
         //retrieve and display entries by keyword
-        cout <<"Which entries would you like to display ? ";
-        cout <<"\n- provide the keyword/category: ";
+        cout <<"Which characters would you like to display ? ";
+        cout <<"\n- provide the skill: ";
         cin.get(temp, 100); cin.ignore(100,'\n');
 
         if(!a_list.retrieve(temp))
             cout << "Could not display entries." << endl;
 
         //retrieve and delete entries based on keyword
-        cout <<"Which entries whould you like to delete ? ";
-        cout <<"\n- provide the keyword/category: ";
+        cout <<"Which characters whould you like to delete ? ";
+        cout <<"\n- provide the skills: ";
         cin.get(temp, 100); cin.ignore(100, '\n');
 
         if(!a_list.remove(temp))
             cout << "Could not display entries." << endl;
 
         //display the BST through inorder traversal
-        cout <<"\nThese are the contents of the APP BST";
+        cout <<"\nThese are the contents of the compendium";
         cout <<"\nin sorted order:\n\n";
         if(!a_list.display_all())
             cout <<"Could not display the APP list." << endl;
 
-        if(keywords) //delete allocated memory for keywords
+        if(skills) //delete allocated memory for skills
         {
-            for(int i = 0; i < cat_num; ++i)
+            for(int i = 0; i < skills_num; ++i)
             {
-                delete [] keywords[i]; // delete each keyword
+                delete [] skills[i]; // delete each keyword
             }
     
-            delete [] keywords; // delete the entire array of pointers
+            delete [] skills; // delete the entire array of pointers
         }
 
     }while(again()); //continue entire program while client wants to continue 
